6c78bcb5e3a3c0d83beb7fa513841ef5da8aaea4
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2023-08-07 15:07:26 +0200

    Setup server with /items and /cart/items endpoints

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import fs from "fs";
+
+const PORT = 8090;
+
+const app = express();
+
+app.use(express.json());
+
+const cartItems = [];
+
+function readItems() {
+    const content = fs.readFileSync("./items.json", "utf8");
+    const json = JSON.parse(content);
+    return json;
+}
+
+app.get("/items", (req, res) => {
+    const items = readItems();
+    res.json(items);
+});
+
+app.get("/cart/items", (req, res) => {
+    res.json(cartItems);
+});
+
+app.post("/cart/items", (req, res) => {
+    cartItems.push(req.body);
+    res.send("Ok");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on:\n  http://localhost:${PORT}`)
+});
